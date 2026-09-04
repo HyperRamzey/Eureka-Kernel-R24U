fc1ba4a0377dc8a20ab93b71e66a9ce2bc96f7cf
HyperRamzey <HyperRamzey@users.noreply.github.com>
2026-09-04 17:54:25 +0300

    Eureka-Kernel R24U sources (e1b8464b) - orphan root for upstreaming work

---
+++ arch/x86/include/asm/bugs.h
@@ -0,0 +1,10 @@
+#ifndef _ASM_X86_BUGS_H
+#define _ASM_X86_BUGS_H
+
+#if defined(CONFIG_CPU_SUP_INTEL) && defined(CONFIG_X86_32)
+int ppro_with_ram_bug(void);
+#else
+static inline int ppro_with_ram_bug(void) { return 0; }
+#endif
+
+#endif /* _ASM_X86_BUGS_H */
